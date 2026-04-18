099c607ee8d90f84efb86b5f001d78236746a262
davids-src <skoda.david@sironic.hu>
2026-04-18 13:45:03 +0200

    refactor: remove icon metadata configuration from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,6 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://sironic.eu"),
-  icons: {
-    icon: [
-      { url: "/sironic_logo.png", type: "image/png", sizes: "512x512" },
-      { url: "/sironic_logo.png", type: "image/png", sizes: "192x192" },
-    ],
-    shortcut: "/sironic_logo.png",
-    apple: "/sironic_logo.png",
-  },
 };
 
 export default function RootLayout({
