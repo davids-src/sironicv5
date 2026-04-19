406b794876b9e0a1f123ed6bab3539bc16ca99e2
davids-src <skoda.david@sironic.hu>
2026-04-19 22:14:07 +0200

    chore: add NextConfig type definition to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,12 @@
+import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 
-const nextConfig = {
+const nextConfig: NextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [],
   },
 };
 
-export default withNextIntl(nextConfig);  
+export default withNextIntl(nextConfig);
